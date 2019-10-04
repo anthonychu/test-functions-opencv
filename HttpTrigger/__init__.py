d18b79b06294254b9092fa22fd14ae2e448497f2
Anthony Chu <antchu@microsoft.com>
2019-10-04 12:14:09 -0700

    Fix indent

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -71,15 +71,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             print ("Verify this a model exported from an Object Detection project.")
 
     # Print the highest probability label
-        highest_probability_index = np.argmax(predictions)
-        print('Classified as: ' + labels[highest_probability_index])
-        print()
+    highest_probability_index = np.argmax(predictions)
+    print('Classified as: ' + labels[highest_probability_index])
+    print()
 
-        # Or you can print out all of the results mapping labels to probabilities.
-        label_index = 0
-        for p in predictions:
-            truncated_probablity = np.float64(np.round(p,8))
-            print (labels[label_index], truncated_probablity)
-            label_index += 1
+    # Or you can print out all of the results mapping labels to probabilities.
+    label_index = 0
+    for p in predictions:
+        truncated_probablity = np.float64(np.round(p,8))
+        print (labels[label_index], truncated_probablity)
+        label_index += 1
 
     return func.HttpResponse(f"{label_index}")
